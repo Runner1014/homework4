d7a3a51c8eea29b5e7dc0ed50b2d65942c216b61
Runner1014 <31625057+Runner1014@users.noreply.github.com>
2018-04-18 00:43:41 +0800

    Add files via upload

---
+++ Assets/Scripts/Controller/Action/CCActionManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CCActionManager : SSActionManager, ISSActionCallback
+{
+
+    public FirstController sceneController;
+    public List<CCFlyAction> Fly;
+    public int DiskNumber = 0; //该round剩余飞碟数
+
+    private List<SSAction> used = new List<SSAction>(); //正在使用的动作
+    private List<SSAction> free = new List<SSAction>(); //还未被激活的动作
+
+    SSAction GetSSAction()
+    {
+        SSAction action = null;
+        if (free.Count > 0)
+        {
+            action = free[0];
+            free.Remove(free[0]);
+        }
+        else
+        {
+            action = Instantiate<CCFlyAction>(Fly[0]);
+        }
+        used.Add(action);
+        return action;
+    }
+
+    public void FreeSSAction(SSAction action)
+    {
+        SSAction tmp = null;
+        foreach (SSAction i in used)
+        {
+            if (action.GetInstanceID() == i.GetInstanceID())
+            {
+                tmp = i;
+            }
+        }
+        if (tmp != null)
+        {
+            tmp.reset();
+            free.Add(tmp);
+            used.Remove(tmp);
+        }
+    }
+
+    protected new void Start()
+    {
+        sceneController = (FirstController)SSDirector.getInstance().currentSceneController;
+        sceneController.actionManager = this;
+        Fly.Add(CCFlyAction.GetSSAction());
+
+    }
+
+    public void SSActionEvent(SSAction source,
+        SSActionEventType events = SSActionEventType.Completed,
+        int intParam = 0,
+        string strParam = null,
+        UnityEngine.Object objectParam = null)
+    {
+        if (source is CCFlyAction)
+        {
+            DiskNumber--;
+            DiskFactory df = Singleton<DiskFactory>.Instance;
+            df.FreeDisk(source.gameobject);
+            FreeSSAction(source);
+        }
+    }
+
+    public void StartThrow(Queue<GameObject> diskQueue)
+    {
+        foreach (GameObject tmp in diskQueue)
+        {
+            RunAction(tmp, GetSSAction(), (ISSActionCallback)this);
+        }
+    }
+}
---
+++ Assets/Scripts/Controller/Action/CCFlyAction.cs
@@ -0,0 +1,53 @@
+﻿/** 
+ * 这个文件是实现飞碟的飞行动作 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCFlyAction : SSAction
+{
+    private float acceleration; //重力加速度=9.8f
+
+    private float horizontalSpeed; //飞碟水平方向的速度
+    private float verticalSpeed;   //飞碟竖直方向的速度
+
+    private Vector3 direction; //飞碟的初始飞行方向
+
+    public override void Start()
+    {
+        enable = true;
+        acceleration = 9.8f;
+        verticalSpeed = 0;
+        horizontalSpeed = gameobject.GetComponent<DiskData>().speed;
+        direction = gameobject.GetComponent<DiskData>().direction;
+    }
+
+    // Update is called once per frame  
+    public override void Update()
+    {
+        if (gameobject.activeSelf)
+        {
+            verticalSpeed += Time.deltaTime * acceleration;
+
+            transform.Translate(direction * horizontalSpeed * Time.deltaTime); //水平运动
+
+            transform.Translate(Vector3.down * verticalSpeed * Time.deltaTime); //竖直运动
+
+            if (transform.position.y < -4)
+            {
+                destroy = true;
+                enable = false;
+                callback.SSActionEvent(this);
+            }
+        }
+
+    }
+
+    public static CCFlyAction GetSSAction()
+    {
+        CCFlyAction action = CreateInstance<CCFlyAction>();
+        return action;
+    }
+}
